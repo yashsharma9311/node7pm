7dcba73d6dad3102558d77dccccea8cbdc950797
yashsharma9311 <yashppsharma26@gmail.com>
2025-11-04 18:39:14 +0530

    change

--- node7pm/server/controllers/empController.js
+++ node7pm/server/controllers/empController.js
@@ -9,32 +9,45 @@ const empSave = async (req, res) => {
     })
     res.send("Data Succesfuuly Save!")
 }
-const empDisplay=async(req, res)=>{
+const empDisplay = async (req, res) => {
     const employee = await EmpModel.find();
     res.send(employee);
 }
 
-const empupdateDisplay=async(req, res)=>{
-     const employee = await EmpModel.find();
-     res.send(employee);
-}
-
-const empupdateDelete=async(req, res)=>{
-  const {id} =req.query;
-   const employee= await EmpModel.findByIdAndDelete(id);
-   res.send("Record Deleted!");
-}
-
-const empSearch=async(req, res)=>{
-    const {empno} = req.body;
-    const employee = await EmpModel.findOne({empno:empno})
+const empupdateDisplay = async (req, res) => {
+    const employee = await EmpModel.find();
     res.send(employee);
 }
 
-const empeditDisplay=async(req, res)=>{
-  const {id} =req.query;
-   const employee = await EmpModel.findById(id);
-   res.send(employee);
+const empupdateDelete = async (req, res) => {
+    const { id } = req.query;
+    const employee = await EmpModel.findByIdAndDelete(id);
+    res.send("Record Deleted!");
+}
+
+const empSearch = async (req, res) => {
+    const { empno } = req.body;
+    const employee = await EmpModel.findOne({ empno: empno })
+    res.send(employee);
+}
+
+const empeditDisplay = async (req, res) => {
+    const { id } = req.query;
+    const employee = await EmpModel.findById(id);
+    res.send(employee);
+}
+
+const editSave = async (req, res) => {
+    const { _id, empno, name, designation, salary } = req.body;
+
+    const employee = await EmpModel.findByIdAndUpdate(_id, {
+        empno: empno,
+        name: name,
+        designation: designation,
+        salary: salary
+    })
+
+    res.send("Data Successfully updated!!");
 }
 
 module.exports = {
@@ -43,5 +56,6 @@ module.exports = {
     empupdateDisplay,
     empupdateDelete,
     empSearch,
-    empeditDisplay
+    empeditDisplay,
+    editSave
 }
